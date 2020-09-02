9430c3cc2681afd9a90052d448d4bd787dd8cedf
Stanisław Chmiela <sjchmiela@gmail.com>
2020-09-02 17:09:27 +0200

    [android] Do not exclude in-app-purchases from AndroidBuildPackages

--- tools/expotools/src/commands/AndroidBuildPackages.ts
+++ tools/expotools/src/commands/AndroidBuildPackages.ts
@@ -195,8 +195,6 @@ async function _updateExpoViewAsync(packages: Package[], sdkVersion: string): Pr
     /TEMPORARY_ABI_VERSION\s*=\s*null/,
     `TEMPORARY_ABI_VERSION = "${sdkVersion}"`
   );
-  await _regexFileAsync(settingsGradle, `// FLAG_BEGIN_REMOVE__UPDATE_EXPOKIT`, `/*`);
-  await _regexFileAsync(settingsGradle, `// FLAG_END_REMOVE__UPDATE_EXPOKIT`, `*/ //`);
   await _uncommentWhenDistributing([appBuildGradle, expoViewBuildGradle]);
   await _commentWhenDistributing([
     constantsJava,
